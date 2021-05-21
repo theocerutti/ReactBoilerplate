36a792858d3a78b790d7bd051d92a41e001e4f58
theocerutti <theo.cerutti@epitech.eu>
2021-05-21 09:36:43 +0200

    Initial commit: setup eslint, prettier, ts, husky...

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+import './assets/css/index.css';
 import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './store/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
---
+++ src/theme.js
@@ -0,0 +1,41 @@
+export const themes = {
+  dark: {},
+  light: {},
+};
+export const defaultTheme = themes.dark;
+
+export const fontSize = {
+  smaller: '10px',
+  small: '12px',
+  base: '14px',
+  large: '16px',
+  larger: '20px',
+  huge: '22px',
+  massive: '24px',
+};
+
+export const fontWeight = {
+  light: '400',
+  base: '400',
+  semibold: '600',
+  bold: '700',
+};
+
+export const padding = {
+  smaller: '4px',
+  small: '8px',
+  base: '16px',
+  large: '20px',
+  larger: '24px',
+};
+
+export const zIndex = {
+  tooltip: 9990,
+  notification: 9000,
+  modal: 8500,
+  overlay: 8000,
+  dropdown: 7000,
+  nav: 500,
+  sidebar: 100,
+  base: 1,
+};
